45c80df735d634611f3ac6c7f5078a7f49bcf040
Ivan Ramirez <niviel69@gmail.com>
2026-01-18 19:31:13 -0800

    Add four more utility functions

--- journal.js
+++ journal.js
@@ -1,5 +1,24 @@
-// Greet a user
+// 1. 
 function greet(name) {
   return "Hello, " + name + "!";
 }
 
+// 2. 
+function add(a, b) {
+  return a + b;
+}
+
+// 3.
+function multiply(a, b) {
+  return a * b;
+}
+
+// 4. 
+function isEven(n) {
+  return n % 2 === 0;
+}
+
+// 5. Reverse 
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
